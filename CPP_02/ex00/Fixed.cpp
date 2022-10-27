f0411e46c74bcbd5dc1c60a9183deecdb5735c54
Rida El fagrouch <rel-fagr@e2r11p1.1337.ma>
2022-10-27 09:51:37 +0100

    testing

--- CPP_02/ex00/Fixed.cpp
+++ CPP_02/ex00/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: rel-fagr <rel-fagr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/27 05:14:53 by rel-fagr          #+#    #+#             */
-/*   Updated: 2022/10/27 06:12:06 by rel-fagr         ###   ########.fr       */
+/*   Updated: 2022/10/27 09:24:39 by rel-fagr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,24 @@
 Fixed::Fixed() 
 { 
     this->fixed_number = 0; 
+    std::cout << "Default constructor called" << std::endl;
 }
 
-Fixed::~Fixed() {}
+Fixed::Fixed(const Fixed &other)
+{
+    std::cout << "Copy constructor called" << std::endl;
+}
+
+Fixed::~Fixed() 
+{
+    std::cout << "Destructor called" << std::endl;
+}
 
 Fixed    Fixed::operator=(const Fixed &rhs)
 {
     if (this != &rhs)
     {
+        std::cout << "Copy assignment operator called" << std::endl;
         this->fixed_number = rhs.fixed_number;
     }
     return *this;
--- CPP_02/ex00/main.cpp
+++ CPP_02/ex00/main.cpp
@@ -6,7 +6,21 @@
 /*   By: rel-fagr <rel-fagr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/27 05:11:19 by rel-fagr          #+#    #+#             */
-/*   Updated: 2022/10/27 05:11:22 by rel-fagr         ###   ########.fr       */
+/*   Updated: 2022/10/27 08:08:55 by rel-fagr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Fixed.hpp"
+
+int main( void ) 
+{
+  Fixed a;
+  Fixed b( a );
+  Fixed c;
+  
+  c = b;
+  std::cout << a.getRawBits() << std::endl;
+  std::cout << b.getRawBits() << std::endl;
+  std::cout << c.getRawBits() << std::endl;
+  return 0;
+}
