8a9681817c0a358a8be6057f12d27c2d70e8fee4
Rida El fagrouch <rel-fagr@e2r10p1.1337.ma>
2022-10-28 03:14:13 +0100

    ex00 done

--- CPP_02/ex00/Fixed.cpp
+++ CPP_02/ex00/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: rel-fagr <rel-fagr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/27 05:14:53 by rel-fagr          #+#    #+#             */
-/*   Updated: 2022/10/27 09:24:39 by rel-fagr         ###   ########.fr       */
+/*   Updated: 2022/10/28 03:11:39 by rel-fagr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,26 +14,39 @@
 
 Fixed::Fixed() 
 { 
-    this->fixed_number = 0; 
     std::cout << "Default constructor called" << std::endl;
+    this->fixed_number = 0;
 }
 
 Fixed::Fixed(const Fixed &other)
 {
     std::cout << "Copy constructor called" << std::endl;
+    this->fixed_number = other.getRawBits();
 }
 
-Fixed::~Fixed() 
+Fixed::~Fixed()
 {
     std::cout << "Destructor called" << std::endl;
 }
 
-Fixed    Fixed::operator=(const Fixed &rhs)
+Fixed   &Fixed::operator=(const Fixed &rhs)
 {
     if (this != &rhs)
     {
         std::cout << "Copy assignment operator called" << std::endl;
-        this->fixed_number = rhs.fixed_number;
+        this->fixed_number = rhs.getRawBits();
     }
     return *this;
+}
+
+int Fixed::getRawBits(void) const
+{
+    std::cout << "getRawBits member function called" << std::endl;
+    return fixed_number;
+}
+
+void    Fixed::setRawBits(int const raw)
+{
+    std::cout << "setRawBits member function called" << std::endl;
+    this->fixed_number = raw;
 }
--- CPP_02/ex00/Fixed.hpp
+++ CPP_02/ex00/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: rel-fagr <rel-fagr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/27 05:12:06 by rel-fagr          #+#    #+#             */
-/*   Updated: 2022/10/27 06:20:54 by rel-fagr         ###   ########.fr       */
+/*   Updated: 2022/10/28 02:58:52 by rel-fagr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,12 @@ class Fixed
 {
 private:
     int fixed_number;
-    static const int fractional;
+    static const int fractional = 8;
 public:
     Fixed();
     ~Fixed();
     Fixed(const Fixed &other);
-    Fixed operator=(const Fixed &rhs);
+    Fixed &operator=(const Fixed &rhs);
     int getRawBits(void) const;
     void    setRawBits(int const raw);
 };
