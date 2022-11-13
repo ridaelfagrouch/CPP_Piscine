1a472e2a5c560005a4e828826e5ee26ce92bce7f
Rida El fagrouch <rel-fagr@e2r9p1.1337.ma>
2022-11-13 13:19:16 +0100

    done

--- CPP_04/ex01/Cat.hpp
+++ CPP_04/ex01/Cat.hpp
@@ -3,19 +3,22 @@
 /*                                                        :::      ::::::::   */
 /*   Cat.hpp                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: garra <garra@student.42.fr>                +#+  +:+       +#+        */
+/*   By: rel-fagr <rel-fagr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/11 14:19:14 by rel-fagr          #+#    #+#             */
-/*   Updated: 2022/11/11 17:42:37 by garra            ###   ########.fr       */
+/*   Updated: 2022/11/13 13:18:24 by rel-fagr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
 #include "Animal.hpp"
+#include "Brain.hpp"
 
 class Cat : public Animal
 {
+private:
+    Brain *brain;
 public:
     Cat();
     ~Cat();
--- CPP_04/ex01/Dog.hpp
+++ CPP_04/ex01/Dog.hpp
@@ -3,19 +3,22 @@
 /*                                                        :::      ::::::::   */
 /*   Dog.hpp                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: garra <garra@student.42.fr>                +#+  +:+       +#+        */
+/*   By: rel-fagr <rel-fagr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/11 14:19:22 by rel-fagr          #+#    #+#             */
-/*   Updated: 2022/11/11 17:42:25 by garra            ###   ########.fr       */
+/*   Updated: 2022/11/13 13:17:18 by rel-fagr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
 #include "Animal.hpp"
+#include "Brain.hpp"
 
 class Dog : public Animal
 {
+private:
+    Brain *brain;
 public:
     Dog();
     ~Dog();
