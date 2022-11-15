19c2fab6946ec43d3d1a0b2a340e4e6eea894301
Rida El fagrouch <rel-fagr@e2r8p1.1337.ma>
2022-11-15 12:01:25 +0100

    test

--- CPP_05/ex01/Form.hpp
+++ CPP_05/ex01/Form.hpp
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
+/*   Form.hpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rel-fagr <rel-fagr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/11/13 22:33:51 by garra             #+#    #+#             */
-/*   Updated: 2022/11/14 12:12:53 by rel-fagr         ###   ########.fr       */
+/*   Created: 2022/11/14 12:08:44 by rel-fagr          #+#    #+#             */
+/*   Updated: 2022/11/14 13:24:54 by rel-fagr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 
 #include <iostream>
 #include <exception>
+#include "Bureaucrat.hpp"
 
 class GradeTooHighException : public std::exception
 {
@@ -28,24 +29,23 @@ class GradeTooLowException : public std::exception
 };
 
 
-class Bureaucrat
+class Form
 {
-    private:
-        const std::string	Name;
-        int					Grade;
-    public:
-        Bureaucrat();
-		Bureaucrat(std::string name, int grade);
-        Bureaucrat(const Bureaucrat &other);
-        ~Bureaucrat();
-        Bureaucrat    &operator=(const Bureaucrat &rhs);
-        std::string    getName(void);
-        int     getGrade(void);
-        GradeTooHighException	gradeTooHighException;
-        GradeTooLowException    gradeTooLowException;
-        void    incrementGrade(void);
-        void    decrementGrade(void);
+private:
+    const std::string Name;
+    bool bol;
+    const int Grade_sign; 
+    const int Grade_execute; 
+public:
+    Form();
+    ~Form();
+    Form(const Form &other);
+    Form &operator=(const Form &rhs);
+    GradeTooHighException	gradeTooHighException;
+    GradeTooLowException    gradeTooLowException;
+    const std::string    getName(void);
+    const int     getGrade(void);
+    void    beSigned(Bureaucrat Bureaucrat); 
 };
 
-
-std::ostream& operator<< (std::ostream& out, Bureaucrat &bureaucrat);
+std::ostream& operator<< (std::ostream& out, Form &form);
