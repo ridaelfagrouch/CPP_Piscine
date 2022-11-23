5ca997ce0a37cf57c74f28d465230e8cd4a6a37f
Rida El fagrouch <rel-fagr@e2r9p14.1337.ma>
2022-11-23 15:10:31 +0100

    ex02 day07 done

--- CPP_07/ex02/Array.hpp
+++ CPP_07/ex02/Array.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Array.hpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: garra <garra@student.42.fr>                +#+  +:+       +#+        */
+/*   By: rel-fagr <rel-fagr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/22 18:15:00 by rel-fagr          #+#    #+#             */
-/*   Updated: 2022/11/23 01:08:18 by garra            ###   ########.fr       */
+/*   Updated: 2022/11/23 15:06:22 by rel-fagr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ class Array
 {
 private:
 	T	*array;
-	int	size;
+	int	size_;
 public:
 	Array<T>();
 	Array<T>(unsigned int n);
@@ -32,75 +32,79 @@ public:
 	int	size() const;
 	~Array<T>();
     class OutOfBound: public std::exception {
-		virtual const char* what() const throw()
+		virtual const char* what() const throw();
 	};
 };
 
 template<typename T>
 Array<T>::Array()
 {
+    std::cout << "Array Default constructor called" << std::endl;
     array = NULL;
-	size = 0;
+	size_ = 0;
 }
 
 template<typename T>
 Array<T>::Array(unsigned int n) 
 {
+    std::cout << "Array constructor with parameters called" << std::endl;
     array = new T[n];
-	size = n;
+	size_ = n;
 }
 
 
 template<typename T>
 T &Array<T>::operator[](int index)
 {
-	if (index >= size || index < 0)
+	if (index >= size_ || index < 0)
         throw Array::OutOfBound();
     else
         return (array[index]);
 }
 
-
 template<typename T>
 Array<T>::Array(const Array& other)
 {
-    size = other.size();
-    if (size > 0)
+    std::cout << "Array copy constructor called" << std::endl;
+    size_ = other.size();
+    if (size_ > 0)
     {
-	    array = new T[size]();
-        for (int i = 0; i < size; i++)
+	    array = new T[size_];
+        for (int i = 0; i < size_; i++)
             array[i] = other.array[i];
     }
+    else
+        array = NULL;
 }
 
 template<typename T>
 Array<T>&	Array<T>::operator=(const Array& rhs)
 {
-    if (this == rhs)
-        return (this);
-	size = rhs.size;
-    if (size > 0)
+    if (this == &rhs)
+        return (*this);
+	size_ = rhs.size_;
+    if (size_ > 0)
     {
-        array = new T[size];
+        array = new T[size_];
         int i = -1;
-        while (++i < size)
+        while (++i < size_)
             array[i] = rhs.array[i];
     }
     else
         array = NULL;
-
     return (*this);
 }
 
 template<typename T>
 int    Array<T>::size() const
 {
-    return (size);
+    return (size_);
 }
 
 template<typename T>
 Array<T>::~Array()
 {
+    std::cout << "Array Destructor called" << std::endl;
 	delete [] array;
 }
 
--- CPP_07/ex02/main.cpp
+++ CPP_07/ex02/main.cpp
@@ -3,16 +3,79 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: garra <garra@student.42.fr>                +#+  +:+       +#+        */
+/*   By: rel-fagr <rel-fagr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/22 18:15:03 by rel-fagr          #+#    #+#             */
-/*   Updated: 2022/11/23 00:37:37 by garra            ###   ########.fr       */
+/*   Updated: 2022/11/23 15:08:04 by rel-fagr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Array.hpp"
 
+void test(void)
+{
+    Array<int> intArray1(10);
+    Array<float> floatArray1(5);
+    Array<std::string> stringArray1(12);
+
+    try
+    {
+        Array<int> intArray2(intArray1);
+        Array<int> intArray3;
+    
+        intArray3 = intArray2;
+        std::cout << "int array size : " << intArray1.size() << std::endl;
+        std::cout << "float array size : " << floatArray1.size() << std::endl;
+        std::cout << "string array size : " << stringArray1.size() << std::endl;
+
+        std::cout << "int array 1: " << intArray1.operator[](5) << std::endl;
+        std::cout << "int array 2: " << intArray2.operator[](5) << std::endl;
+        std::cout << "int array 3: " << intArray3.operator[](5) << std::endl;
+        std::cout << intArray1.operator[](12) << std::endl;
+    }
+    catch(std::exception& e)
+    {
+        std::cout << "int Array index " << e.what() << '\n';
+    }
+    
+    std::cout << std::endl;
+    try
+    {
+        Array<float> floatArray2(floatArray1);
+        Array<float> floatArray3;
+    
+        floatArray3 = floatArray2;
+        std::cout << "float array 1: " << floatArray1.operator[](2) << std::endl;
+        std::cout << "float array 2: " << floatArray2.operator[](2) << std::endl;
+        std::cout << "float array 3: " << floatArray3.operator[](2) << std::endl;
+        std::cout << floatArray1.operator[](10) << std::endl;
+    }
+    catch(std::exception& e)
+    {
+        std::cout << "float Array index " << e.what() << '\n';
+    }
+
+    std::cout << std::endl;
+    try
+    {
+        Array<std::string> stringArray2(stringArray1);
+        Array<std::string> stringArray3;
+    
+        stringArray3 = stringArray2;
+        std::cout << "string array 1: " << stringArray1.operator[](4) << std::endl;
+        std::cout << "string array 2: " << stringArray2.operator[](4) << std::endl;
+        std::cout << "string array 3: " << stringArray3.operator[](4) << std::endl;
+        std::cout << stringArray1.operator[](15) << std::endl;
+    }
+    catch(std::exception& e)
+    {
+        std::cout << "string Array index " << e.what() << '\n';
+    }
+    std::cout << std::endl;
+}
+
 int main()
 {
-    ;
+    test();
+    system("leaks Array");
 }
